357bd66bf7126fa3ac921f05c0107ecc81ce61a9
Adin Farel <fareladin00@gmail.com>
2026-05-24 22:24:23 +0700

    feat: add block transformer

---
+++ basemodel/src/model/block.py
@@ -0,0 +1,31 @@
+'''
+src/model/block.py
+
+A single block transformer that contains all piece of modern Architecture BERT
+'''
+
+import torch
+import torch.nn as nn
+
+from basemodel.src.model import(
+    ffwd,
+    mha
+)
+
+class Block(nn.Module):
+    def __init__(self, n_embd: int, n_heads: int, dropout: float):
+        super().__init__()
+        self.attn = mha.MultiHeadAttention(
+            embed_dim=n_embd,
+            n_heads=n_heads,
+            dropout=dropout
+        )
+        self.pre_ln1 = nn.LayerNorm(n_embd)
+        self.ffwd = ffwd.GeGLU(embed_dim=n_embd, dropout=dropout)
+        self.pre_ln2 = nn.LayerNorm(n_embd)
+    
+    def forward(self, x: torch.Tensor, attn_mask: torch.Tensor | None = None):
+        # Why x + ... ? it is method to prevent vanishing gradient, name method is ResNet 
+        x = x + self.attn(self.pre_ln1(x), attn_mask=attn_mask)
+        x = x + self.ffwd(self.pre_ln2(x))
+        return x
